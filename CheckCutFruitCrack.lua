ac68df8a6c9dd292cb79c55c31a7597a94f4e966
lovehappy108 <apple@MacBook-Pro.local>
2016-06-18 16:44:58 +0800

    update 1.3.19
    update 1.3.19

--- CheckCutFruitCrack.lua
+++ CheckCutFruitCrack.lua
@@ -1,7 +1,7 @@
 return {
 	["isDoor"] = true;
 	["android"] = {
-		["1.3.19"] = "b62420c956f1acae3e98f596a2ef4e55";
+		["1.3.19"] = "a8357859e10334bacb1eafa85b38ea61";
 	};
 	["ios"] = {
 		["1.3.18"] = nil;
