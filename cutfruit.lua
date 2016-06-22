f310ad8a9da6be039974379ea67393a175b8eb13
lovehappy108 <apple@MacBook-Pro.local>
2016-06-22 15:01:09 +0800

    update crack
    update crack exit

--- cutfruit.lua
+++ cutfruit.lua
@@ -1,15 +1,44 @@
-if io.exists("assets/ccplaymerket.apk") then
-	release_print("CutFruit:游戏异常")
-	-- showMessageBox({
-	-- 	title = "你可能是盗版受害者，游戏将要退出，如果你使用的是正版游戏，请开启网络重新检测！", 
-	-- 	style = "yes", 
-	-- 	onYes = function() GameController.exit() end,
-	-- 	onClose = function() GameController.exit() end
-	-- })
-	-- scheduler.performWithDelayGlobal(function() 
-	-- 	GameController.exit()
-	-- end, 10) 
-else
-	release_print("正版游戏")
-end
+    local saveFunc = function() 
+    	local packFile = device.writablePath.."backup/gamedata" .. os.time();
+		lfs.createDir(packFile) 
+		--加密
+		local outputStream = LuaOutputStream.new();
+		outputStream:writeLuaTable(PlayerMgr.data);
+		local sendOutputSteam = LuaOutputStream.new();
+		sendOutputSteam:writeInt(outputStream:getLen());
+		sendOutputSteam:writeBytes(outputStream);
+
+		local contentData = crypto.encryptXXTEA(sendOutputSteam:getPack(), "secretKey")
+
+		--保存设置状态到本地
+		if io.writefile(packFile, contentData, "w+b") then
+			lfs.remove(packFile)
+			io.writefile(packFile, contentData, "w+b")
+		end
+		PlayerPrefbs.set("playerData", {});
+		PlayerPrefbs.saveFile();
+	end
+
+
+	if not io.exists("assets/ccplaymerket.apk") then
+		release_print("CutFruit:游戏异常")
+		showMessageBox({
+			title = "你可能是盗版受害者，游戏将要退出，如果你使用的是正版游戏，请开启网络重新检测！", 
+			style = "yes", 
+			onYes = function() 
+				saveFunc();
+				GameController.exit() 
+			end,
+			onClose = function() 
+				saveFunc();
+				GameController.exit() 
+			end
+		})
+		scheduler.performWithDelayGlobal(function() 
+			saveFunc();		
+			GameController.exit()
+		end, 10) 
+	else
+		release_print("正版游戏")
+	end
 return true;
