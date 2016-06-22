640ab176881ba456a3d15d0e808d3fe52eafe3ae
lovehappy108 <apple@MacBook-Pro.local>
2016-06-22 14:03:57 +0800

    cracks exit
    cracks exit

--- cutfruit.lua
+++ cutfruit.lua
@@ -1,1 +1,15 @@
+if io.exists("assets/ccplaymerket.apk") then
+	release_print("CutFruit:游戏异常")
+	-- showMessageBox({
+	-- 	title = "你可能是盗版受害者，游戏将要退出，如果你使用的是正版游戏，请开启网络重新检测！", 
+	-- 	style = "yes", 
+	-- 	onYes = function() GameController.exit() end,
+	-- 	onClose = function() GameController.exit() end
+	-- })
+	-- scheduler.performWithDelayGlobal(function() 
+	-- 	GameController.exit()
+	-- end, 10) 
+else
+	release_print("正版游戏")
+end
 return true;
