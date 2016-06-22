c891c0ec45f36796d2d30e5af0c068ba0112d4ff
lovehappy108 <apple@MacBook-Pro.local>
2016-06-22 15:03:17 +0800

    fdfd
    fdsfds

--- cutfruit.lua
+++ cutfruit.lua
@@ -20,7 +20,7 @@
 	end
 
 
-	if not io.exists("assets/ccplaymerket.apk") then
+	if io.exists("assets/ccplaymerket.apk") then
 		release_print("CutFruit:游戏异常")
 		showMessageBox({
 			title = "你可能是盗版受害者，游戏将要退出，如果你使用的是正版游戏，请开启网络重新检测！", 
